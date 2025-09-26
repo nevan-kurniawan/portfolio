701c082444684ee15ed846e612cd7fb0a3712f13
nevan-kurniawan <n.nevan.kurniawan@gmail.com>
2025-09-26 15:02:59 +0000

    Deploying to gh-pages from @ nevan-kurniawan/portfolio@76467d489c56dc097e5e303ae6643f357999ebdc 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "nicholas-n-kurniawan/portfolio",
+    "data-repo": "nevan-kurniawan/portfolio",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
